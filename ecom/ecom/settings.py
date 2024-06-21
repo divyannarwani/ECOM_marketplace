526cc150520789588b1d507ec9ed287102571bd5
divyannarwani <divyannarwani14@gmail.com>
2024-06-21 19:28:54 +0530

    Ready for production

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,12 @@ SECRET_KEY = 'django-insecure--1$8_$^kl@txfnu*gw+czbv_o0&iv%@@vz#=sy&hv&x346-w&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS.extend([os.environ.get('VERCEL_URL', '')])
+
+
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
@@ -124,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
